15d569e38999216d33406665689daefe9e27052c
DannyM1chael <coldsoulrusirk@gmail.com>
2020-08-03 12:28:22 +0300

    Updates

---
+++ precache-manifest.965865535e387ff830cba4938ab33582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2fda48a0141ce1bc91aa03ba4a53422",
+    "url": "/Portfolio-app/index.html"
+  },
+  {
+    "revision": "d8835884e344c4cb27e2",
+    "url": "/Portfolio-app/static/css/main.930ced27.chunk.css"
+  },
+  {
+    "revision": "030e19bfd59bfeb4b1df",
+    "url": "/Portfolio-app/static/js/2.e687f541.chunk.js"
+  },
+  {
+    "revision": "7697640d703ae35e4d2120f28e9ccbdd",
+    "url": "/Portfolio-app/static/js/2.e687f541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8835884e344c4cb27e2",
+    "url": "/Portfolio-app/static/js/main.ac58107a.chunk.js"
+  },
+  {
+    "revision": "1dfb291467a2a9cffbd6",
+    "url": "/Portfolio-app/static/js/runtime-main.06fef774.js"
+  }
+]);
